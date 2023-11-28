1c7535149f9fc060d1197e6ece4425f41b26e5f7
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-11-28 17:04:52 +0200

    Task 03 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import _ from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on(
+  'timeupdate',
+  _.throttle(function (time) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+  }, 1000)
+);
+
+player.on('loaded', function () {
+  const timeCode = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  ) ?? { seconds: 0 };
+  player.setCurrentTime(timeCode.seconds);
+});
