4e8a80274b0155ee595d578ac1bfd215e4ee3bc1
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-11-29 14:30:05 +0000

    Deploying to gh-pages from @ DmytroLynnyk/goit-js-hw-08@687932ac24d723b960a6bed0a7de58254b59083b 🚀

---
+++ 03-feedback.3252584d.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form"),document.querySelector("submit"),form.querySelector('[name="email"]'),form.querySelector('[name="message"]');
+//# sourceMappingURL=03-feedback.3252584d.js.map
