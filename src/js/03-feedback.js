687932ac24d723b960a6bed0a7de58254b59083b
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-11-29 16:29:39 +0200

    task 03 not done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('submit');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
 
+// feedbackForm.addEventListener('input', onInput);
+// feedbackForm.addEventListener('submit', onSubmit);
+// submitBtn.addEventListener('submit', onSubmit);
