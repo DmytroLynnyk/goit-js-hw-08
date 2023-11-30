11de9ff18283bdaefa8e7b4ee7170950ead213a4
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-11-30 12:01:33 +0200

    Task 03 without check of storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,32 @@
-const feedbackForm = document.querySelector('.feedback-form');
-const submitBtn = document.querySelector('submit');
-const emailInput = form.querySelector('[name="email"]');
-const messageInput = form.querySelector('[name="message"]');
+import { throttle } from 'throttle-debounce';
 
-// feedbackForm.addEventListener('input', onInput);
-// feedbackForm.addEventListener('submit', onSubmit);
-// submitBtn.addEventListener('submit', onSubmit);
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('input', onInput);
+feedbackForm.addEventListener('submit', onSubmit);
+
+const throttleFunc = throttle(500, feedbackFormState => {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+});
+
+function onInput(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const feedbackFormState = {
+    email: email.value,
+    message: message.value,
+  };
+  throttleFunc(feedbackFormState);
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  feedbackForm.reset();
+  localStorage.removeItem('feedback-form-state');
+}
